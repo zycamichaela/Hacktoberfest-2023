f46604a336fb12fb63801e2573c9016cf4160445
Faishal Munir <ragnanear@gmail.com>
2020-10-05 00:02:20 +0700

    Update and rename Hello World .md to Hello World .py
    example

--- scripts/Hello World .md
+++
@@ -1,7 +0,0 @@
-#contoh
-// LANGUAGE: Javascript
-// ENV: Node.js
-// AUTHOR: Zyca Michaela
-// GITHUB: https://github.com/mfaisaal
-
-console.log("Hello World") 
---
+++ scripts/Hello World .py
@@ -0,0 +1,6 @@
+#Lang Python2
+#env Interactive as well as script
+#aut zycamichaela
+#github https://github.com/mfaisaal
+print "Hello World"
+print "iam was here to Say Hello World!"
