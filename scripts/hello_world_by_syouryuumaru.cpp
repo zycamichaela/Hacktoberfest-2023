c8d9f85426e47c566506263a579d7e68ddc8e4d0
Yeh Shuhua <sinonsheele@gmail.com>
2020-10-05 00:52:56 +0700

    thanks

---
+++ scripts/hello_world_by_syouryuumaru.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+// LANGUAGE: C++
+// AUTHOR: Chad P. Wilson
+// GITHUB: https://github.com/syouryuumaru
+// Hello_world_script_with_C
+// thanks
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
