b4b52cf23910db45c02312bb9d03494a27950553
Fahmiady37 <asfahmi5@gmail.com>
2020-10-15 15:01:26 +0700

    Add it hacktoberfest

--- scripts/Hello_word_fahmiady11.py
+++ scripts/Hello_word_fahmiady11.py
@@ -1,5 +1,5 @@
-// LANGUAGE: Python
-// AUTHOR: fahmi ady
-// GITHUB: https: // github.com/Fahmiady11
+# LANGUAGE: Python
+# AUTHOR: fahmi ady
+# GITHUB: https: // github.com/Fahmiady11
 
 print("hello word")
