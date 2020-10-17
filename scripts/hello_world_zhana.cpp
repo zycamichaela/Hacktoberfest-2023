cef4b3f3a51cf4f18218ad80fd7bdad34bad8d88
sinons <sinonsheele@gmail.com>
2020-10-18 00:24:34 +0700

    "add it hacktoberfest"

---
+++ scripts/hello_world_zhana.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+// LANGUAGE: C++
+// AUTHOR: Zhana
+// GITHUB: https://github.com/nearfoe1
+// Hello_world_script_with_C
+// thank you
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
